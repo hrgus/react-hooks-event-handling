56f67c0e2f5586b068a9d76327eeb7f80cdf4b4f
Harry Gustafson <hrgus3@gmail.com>
2022-02-01 07:47:16 -0500

    Assignment Complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,8 +8,14 @@ function Login() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="username" placeholder="Enter username..." />
-      <input type="password" name="password" placeholder="Enter password..." />
+      <input 
+      type="text"
+      name="username"
+      placeholder="Enter username..." />
+      <input 
+      type="password"
+      name="password"
+      placeholder="Enter password..." />
       <button>Login</button>
     </form>
   );
